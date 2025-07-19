c04fae631f23eee6cfd473cb91086be73d87c819
Alain Lehmann <alain.lehmann@gmail.com>
2025-07-19 23:59:48 +0200

    update: nixpkgs 25.05 => unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "NixOS on Lima Module";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05-small";
+    #nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05-small";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
   };
