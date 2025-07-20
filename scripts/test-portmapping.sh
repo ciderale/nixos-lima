1063e38b95bbb89a235afb35427a9ffd36cd23a7
Alain Lehmann <alain.lehmann@gmail.com>
2025-07-20 23:28:32 +0200

    test-portmapping: concurrent runs

--- scripts/test-portmapping.sh
+++ scripts/test-portmapping.sh
@@ -1,13 +1,27 @@
 #!/usr/bin/env bash
 
-PORT=8888
+PORT=${1:-8888}
+RUNS=${2:-1}
 IMAGE=alpine/socat
 MESSAGE="SUCCESSFUL RESPONSE"
+
+if [ "$RUNS" -gt 1 ]; then
+  RESULTS=$(
+    (for ((p=PORT; p<PORT+RUNS; p++)); do
+      sleep 0.3;
+      "$0" "$p" 1 &
+      done
+    ) | sort -n
+  )
+  echo "########################"
+  echo "Excution durations [ms]:"
+  echo "$RESULTS"
+  exit
+fi
 
 function now() {
   echo $(($(date +%s%N)/1000000))
 }
-
 
 START=$(now)
 
@@ -17,18 +31,18 @@ function elapsed() {
 }
 
 function log() {
-  printf "Elapsed: %6d ms: $1\n" "$(elapsed)"
+  printf "Elapsed: %6d ms: $1\n" "$(elapsed)" >&2
 }
 
 
-log "Starting socat http server"
-docker run -p $PORT:$PORT $IMAGE \
-  TCP-LISTEN:$PORT,crlf,reuseaddr,fork \
+log "Starting socat http server $PORT"
+docker run -p "$PORT:$PORT" "$IMAGE" \
+  "TCP-LISTEN:$PORT,crlf,reuseaddr,fork" \
   SYSTEM:"echo HTTP/1.0 200; echo; echo '$MESSAGE'" &
 
 log "Server start in background. Start Requests.."
 
-while ! curl --fail --silent localhost:$PORT | grep -- "$MESSAGE"; do
+while ! curl --fail --silent "localhost:$PORT" | grep -- "$MESSAGE" >&2; do
   log "failed request"
   sleep 0.02
 done
@@ -40,3 +54,4 @@ kill %1
 wait
 log "DONE"
 echo "$RESPONSIVITY"
+
