5ab0a44a6d9177d8da45bc7e0eead9a096b48352
Alain Lehmann <alain.lehmann@gmail.com>
2025-08-02 22:00:33 +0200

    add test-portmapping UDP&TCP

--- scripts/test-portmapping.sh
+++ scripts/test-portmapping.sh
@@ -4,12 +4,16 @@ PORT=${1:-8888}
 RUNS=${2:-1}
 IMAGE=alpine/socat
 MESSAGE="SUCCESSFUL RESPONSE"
+PROTOCOL=${3:-TCP}
+
+#SOCAT_FLAGS=(-d -d -lu) for debugging
+SOCAT_FLAGS=()
 
 if [ "$RUNS" -gt 1 ]; then
   RESULTS=$(
     (for ((p=PORT; p<PORT+RUNS; p++)); do
-      sleep 0.3;
-      "$0" "$p" 1 &
+      sleep 0.3; # minor delay to get a uniform load over time
+      "$0" "$p" 1 "$PROTOCOL" &
       done
     ) | sort -n
   )
@@ -35,21 +39,32 @@ function log() {
 }
 
 
+date -Ins >&2
 log "Starting socat http server $PORT"
-DOCKER_ID=$(docker run --rm -d -p "$PORT:$PORT" "$IMAGE" \
-  "TCP-LISTEN:$PORT,reuseaddr,fork" \
+DOCKER_ID=$(docker run --rm -d -p "$PORT:$PORT/$PROTOCOL" "$IMAGE" \
+  "${SOCAT_FLAGS[@]}" \
+  "$PROTOCOL-LISTEN:$PORT,reuseaddr,fork" \
   EXEC:cat,nofork)
 trap 'docker kill $DOCKER_ID > /dev/null' EXIT
 
 log "Server start in background. Start Requests.."
+date -Ins >&2
+START=$(now)
 
-# sleep after the echo so that socat does not exit before reading the response
-while ! (echo "$MESSAGE"; sleep 0.02) | socat - "TCP:localhost:$PORT" | grep -- "$MESSAGE" >&2; do
+# the backgrounded sleep is to avoid closing stdin to early
+# this ensures that the response from socat is received
+while ! (sleep 0.02 & echo "$MESSAGE") | socat "${SOCAT_FLAGS[@]}" - "$PROTOCOL:localhost:$PORT" | grep -- "$MESSAGE" >&2; do
   log "failed request"
 done
 
 RESPONSIVITY=$(elapsed)
 log "### SUCCESSFULL REQUEST ###"
+date -Ins >&2
+
+if [ -n "${SOCAT_FLAGS[*]}" ]; then
+  log "### show docker logs"
+  docker logs "$DOCKER_ID"
+fi
 
 log "DONE"
 echo "$RESPONSIVITY"
