dbd10953f00be612894b15c6fe07a83165c68f38
Alain Lehmann <alain.lehmann@gmail.com>
2025-11-08 15:19:04 +0100

    update to lima 2.0.1

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,6 +1,14 @@
 final: prev: {
   # support SIGHUP for portmapping update
-  lima = prev.lima.overrideAttrs (old: {
-    patches = [./lima-sighup.patch];
+  lima = prev.lima.overrideAttrs (old: rec {
+    version = "2.0.1";
+    #patches = [./lima-sighup.patch];
+    src = prev.fetchFromGitHub {
+      owner = "lima-vm";
+      repo = "lima";
+      tag = "v${version}";
+      hash = "sha256-GPrx4pvD6AxYIvAS+Mz8gFZ/Z7HeFFoHh3LOtAJ9bhI=";
+    };
+    vendorHash = "sha256-dA6zdrhN73Y8InlrCEdHgYwe5xbUlvKx0IMis2nWgWE=";
   });
 }
