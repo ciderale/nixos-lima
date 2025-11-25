3fb46e03c4d8103844fef0238245909a110028bc
Alain Lehmann <alain.lehmann@gmail.com>
2025-11-25 19:55:26 +0100

    update nixpkgs, lima 2.0.2

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,14 +1,13 @@
 final: prev: {
   # support SIGHUP for portmapping update
   lima = prev.lima.overrideAttrs (old: rec {
-    version = "2.0.1";
-    #patches = [./lima-sighup.patch];
+    version = "2.0.2";
     src = prev.fetchFromGitHub {
       owner = "lima-vm";
       repo = "lima";
       tag = "v${version}";
-      hash = "sha256-GPrx4pvD6AxYIvAS+Mz8gFZ/Z7HeFFoHh3LOtAJ9bhI=";
+      hash = "sha256-MWNvHHyf2qZxt83D22tTKR6EXAeAgcoXE1YjXHc9SwQ=";
     };
-    vendorHash = "sha256-dA6zdrhN73Y8InlrCEdHgYwe5xbUlvKx0IMis2nWgWE=";
+    vendorHash = "sha256-fCqAf0buqA6GajP7SIsVPyKM3jY2n9CbS5hpa3dsWbc=";
   });
 }
