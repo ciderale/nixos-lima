9fca3668521194eff2982bd21228ad6c6913fb13
Alain Lehmann <alain.lehmann@gmail.com>
2025-11-27 00:26:17 +0100

    cleanup example

--- example/lima-settings.nix
+++ example/lima-settings.nix
@@ -3,29 +3,16 @@
   lib,
   ...
 }: {
+  documentation.enable = false; # saves about 0.1GB
+
   #lima.settings.plain = true; #disables mounts,ports,ga,etc
-  # lima.tick = "1000ms"; # more polling increases system load
   lima.settings.ssh.localPort = 2222;
   lima.settings.mounts = [
     {
       location = "/Users/${config.lima.user.name}";
       writable = true;
     }
-    {
-      location = "/tmp/lima";
-      writable = true;
-    }
   ];
-  lima.settings.portForwards = [
-    {
-      guestIP = "127.0.0.1";
-      proto = "any"; # tcp and udp
-    }
-  ];
-  lima.settings.hostResolver = {
-    enabled = true;
-    hosts."host.docker.internal" = "host.lima.internal";
-  };
   #lima.settings.video.display = "vz"; add gui
   lima.settings.memory = lib.mkDefault "8GB";
   lima.settings.cpus = lib.mkDefault 8;
@@ -40,6 +27,10 @@
     # --add-host host.docker.internal=host-gateway yields ip of host not vm
     host-gateway-ips = [config.lima.hostLimaInternal];
   };
+  lima.settings.hostResolver = {
+    enabled = true;
+    hosts."host.docker.internal" = "host.lima.internal";
+  };
   # virtualisation.podman.enable = true;
-  documentation.enable = false; # saves about 0.1GB
+  #nix.settings.trusted-users = ["ale"];
 }
