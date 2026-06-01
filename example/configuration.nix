d425eb2df0ec3f4d60f2690ce2166e1c14390e04
Alain Lehmann <alain.lehmann@gmail.com>
2026-06-01 19:27:48 +0200

    set timezone in example configuration

--- example/configuration.nix
+++ example/configuration.nix
@@ -5,6 +5,7 @@
 }: {
   system.stateVersion = "24.11";
   nix.settings.experimental-features = ["nix-command" "flakes"];
+  time.timeZone = "Europe/Zurich";
 
   environment.systemPackages = map lib.lowPrio [
     pkgs.vim
