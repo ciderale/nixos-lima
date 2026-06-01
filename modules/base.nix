2fc31538c01b142e931af25ad75cffd2caa8860c
Alain Lehmann <alain.lehmann@gmail.com>
2026-06-01 19:33:23 +0200

    use systemd.network instead of dhcpd

--- modules/base.nix
+++ modules/base.nix
@@ -9,4 +9,16 @@
     efiSupport = true;
     efiInstallAsRemovable = true;
   };
+
+  # note: networking.useDHCP keeps 0.0.0.0:68 bound permanently, while systemd.network rebinds to IP:68 after lease
+  # this avoid that :68 is bound by the lima/host-agent on the host, which cause problems
+  networking.useDHCP = false;
+  networking.useNetworkd = true;
+  systemd.network = {
+    enable = true;
+    networks."10-enp0s1" = {
+      matchConfig.Name = "enp0s1";
+      networkConfig.DHCP = "ipv4";
+    };
+  };
 }
